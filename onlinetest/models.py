1d3cb6f27fd67cba8f0aace535cb79fd753b59ca
realsdx <realsdx@protonmail.com>
2019-11-01 18:15:52 +0530

    Update models and views

--- onlinetest/models.py
+++ onlinetest/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Question(models.Model):
@@ -20,11 +22,20 @@ class Answer(models.Model):
         return str_repr
 
 
-class Student(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     time_elapsed = models.IntegerField(default=0)
     phone = models.CharField(max_length=14)
     rollno = models.CharField(max_length=10)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
     def __str__(self):
         return self.rollno
--- onlinetest/views.py
+++ onlinetest/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from onlinetest.models import Question
 
 # Create your views here.
 def index(req):
     return render(req, 'onlinetest/index.html')
 
+# @login_required
 def questions(req):
-    return render(req, 'onlinetest/questions.html')
+    if req.method == "POST":
+        pass
+    else:
+        return render(req, 'onlinetest/questions.html')
 
 def rules(req):
     return render(req, 'onlinetest/rules.html')
