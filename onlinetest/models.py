577cae5191806ea3b5f19100dea6c759a5e6af92
realsdx <realsdx@protonmail.com>
2019-11-01 17:55:41 +0530

    Update models

--- onlinetest/models.py
+++ onlinetest/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Question(models.Model):
-    title = models.CharField(max_length=1024)    
+    title = models.CharField(max_length=1024)
+
     def __str__(self):
         return self.title
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
@@ -13,15 +15,16 @@ class Answer(models.Model):
     text = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.question.title
+        str_repr = self.text[:51] + \
+            "..." if (len(self.text) >= 52) else self.text
+        return str_repr
+
 
 class Student(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    time_elapsed = models.IntegerField(default=0)
     phone = models.CharField(max_length=14)
     rollno = models.CharField(max_length=10)
 
     def __str__(self):
         return self.rollno
-
-
-
