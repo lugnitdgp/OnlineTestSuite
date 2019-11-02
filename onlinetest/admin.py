9d7006e6f25a49106f3439e53f7f57e7ec44d177
realsdx <realsdx@protonmail.com>
2019-11-02 21:57:42 +0530

    add custom admin manager for Profile

--- onlinetest/admin.py
+++ onlinetest/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from onlinetest.models import Question, Answer, Profile
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name','user','time_left']
+    actions = ['increase_time_by_10']
+
+    def increase_time_by_10(self, req, queryset):
+        for profile in queryset:
+            profile.time_left += 600
+            profile.save()
+    
+    increase_time_by_10.short_description = "Increase time by 10 min"
+
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
