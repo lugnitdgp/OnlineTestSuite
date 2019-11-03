305242017b0cd20a148c6a96ee96e9314ef22c69
realsdx <realsdx@protonmail.com>
2019-11-03 14:07:36 +0530

    update student response view admin page

--- onlinetest/admin.py
+++ onlinetest/admin.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 from onlinetest.models import Question, Answer, Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-
-    change_form_template = "admin/change_view.html"
+    change_form_template = "admin/custom_change_form.html"
 
     list_display = ['full_name','user','time_left']
     actions = ['increase_time_by_10']
@@ -18,36 +17,26 @@ class ProfileAdmin(admin.ModelAdmin):
 
     def get_dynamic_info(self, object_id):
         questions = Question.objects.all()
-        # for q in questions:
-        #     print(q)
         p = Profile.objects.get(id=object_id)
-        u = User.objects.get(username=p.user.username)
-        # print(u)
+        answers = Answer.objects.filter(user=p.user)
+    
         already_submitted = []
         for q in questions:
-            already_submitted.append(Answer.objects.filter(question=q, user=u).values('text'))
+            ans = answers.filter(question=q).first()
+            if ans:
+                already_submitted.append(ans.text)
+            else:
+                already_submitted.append("N/A")
 
-        # print(len(already_submitted))
         respon = []
         for i in range(max(len(questions), len(already_submitted))):
             respon.append([questions[i], already_submitted[i]])
-        for r in respon:
-            if(r[1]):
-                for i in r[1]:
-                    r[0] = r[0].title
-                    r[1] = i['text']
-                    # print("{} {}".format(r[0], i['text']))
-            else:
-                r[0] = r[0].title
-                r[1] = "NA"
-                # print("{} {}".format(r[0], "NA"))
 
         return respon
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         extra_context = extra_context or {}
-        extra_context['osm_data'] = self.get_dynamic_info(object_id=object_id)
-        # print(extra_context)
+        extra_context['responses'] = self.get_dynamic_info(object_id=object_id)
         return super(ProfileAdmin, self).change_view(request, object_id, form_url, extra_context=extra_context)
 
 
