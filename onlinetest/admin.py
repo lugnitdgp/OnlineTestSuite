7cec1988b64f029592a8fab1cb2415be9fcbf9d7
Sudipta Pandit <realsdx@protonmail.com>
2019-11-03 11:59:59 +0530

    Revert "Update Profile admin change_view"
    This reverts commit 07221bdbe16094cedce84e1dbb2e72bdf8023ae3.

--- onlinetest/admin.py
+++ onlinetest/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from onlinetest.models import Question, Answer, Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-
-    change_form_template = "admin/change_view.html"
-
     list_display = ['full_name','user','time_left']
     actions = ['increase_time_by_10']
 
@@ -14,40 +11,6 @@ class ProfileAdmin(admin.ModelAdmin):
             profile.save()
     
     increase_time_by_10.short_description = "Increase time by 10 min"
-
-    def get_dynamic_info(self, object_id):
-        questions = Question.objects.all()
-        # for q in questions:
-        #     print(q)
-        u = User.objects.get(id=object_id)
-        # print(u)
-        already_submitted = []
-        for q in questions:
-            already_submitted.append(Answer.objects.filter(question=q, user=u).values('text'))
-
-        # print(len(already_submitted))
-        respon = []
-        for i in range(max(len(questions), len(already_submitted))):
-            respon.append([questions[i], already_submitted[i]])
-        for r in respon:
-            if(r[1]):
-                for i in r[1]:
-                    r[0] = r[0].title
-                    r[1] = i['text']
-                    # print("{} {}".format(r[0], i['text']))
-            else:
-                r[0] = r[0].title
-                r[1] = "NA"
-                # print("{} {}".format(r[0], "NA"))
-
-        return respon
-
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-        extra_context = extra_context or {}
-        extra_context['osm_data'] = self.get_dynamic_info(object_id=object_id)
-        # print(extra_context)
-        return super(ProfileAdmin, self).change_view(request, object_id, form_url, extra_context=extra_context)
-
 
 admin.site.register(Question)
 admin.site.register(Answer)
